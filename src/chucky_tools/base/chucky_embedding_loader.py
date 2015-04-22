ff265b94fc5dcd2951203508e75592d2124b5af7
Alwin Maier <alwin.maier@stud.uni-goettingen.de>
2015-04-22 13:00:33 +0200

    Update base tool for loading embeddings.
    Update base tool for loading embeddings.
    
     - group options
    
     - exceptions while loading the embedding are no longer handled by this
       class
    
     - fix mapping from columns (feature matrix) to dimensions (embedding)

--- src/chucky_tools/base/chucky_embedding_loader.py
+++ src/chucky_tools/base/chucky_embedding_loader.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 
 from sklearn.datasets import load_svmlight_file
@@ -8,7 +7,7 @@ from chucky_tools.base import ChuckyLogger
 
 class ChuckyEmbeddingLoader(ChuckyLogger):
     """
-    A basic tool for loading an embedding in libsvm/svmlight format.:wq
+    A basic tool for loading an embedding in libsvm/svmlight format.
     """
 
     def __init__(self, description):
@@ -16,34 +15,37 @@ class ChuckyEmbeddingLoader(ChuckyLogger):
         self._x = None
         self._y = None
         self._toc = None
+        self._svd = None
+        self._column_to_dimension = None
         self.__is_initialized = False
 
     def _initializeOptParser(self):
         super(ChuckyEmbeddingLoader, self)._initializeOptParser()
-        self.argParser.add_argument(
+        group = self.argParser.add_argument_group("embedding options")
+        group.add_argument(
             'embedding',
             type=str,
             help="directory containing the embedding"
         )
-        self.argParser.add_argument(
+        group.add_argument(
             '--toc',
             type=str,
             default=None,
-            help="use toc file as labels"
+            help="use labels from toc file"
         )
 
     def _load_embedding(self):
-        try:
-            self._x, self._y = load_svmlight_file(self.args.embedding)
-            self._toc = self._create_toc()
-            self.logger.info("Loaded dimensions: {}".format(self._x.shape[1]))
-            columns = np.unique(self._x.nonzero()[1])
-            self._x = self._x[:, columns]
-            self.logger.info("Nonzero dimensions: {}".format(self._x.shape[1]))
-            self.__is_initialized = True
-        except Exception as e:
-            self.logger.error('Failed while loading embedding: %s', e.message)
-            sys.exit(1)
+        self._x, self._y = load_svmlight_file(self.args.embedding)
+
+        self._toc = self._create_toc()
+
+        columns = np.unique(self._x.nonzero()[1])
+        self._column_to_dimension = dict(zip(range(len(columns)), columns))
+        self._x = self._x[:, columns]
+
+        self.__is_initialized = True
+        self.logger.info("Loaded datapoints: {}".format(self.number_of_datapoints))
+        self.logger.info("Loaded non-zero dimensions: {}".format(self.number_of_features))
 
     def feature_matrix(self, datapoints):
         rows = [self.get_index(x) for x in datapoints]
@@ -61,6 +63,9 @@ class ChuckyEmbeddingLoader(ChuckyLogger):
 
     def get_index(self, node_id):
         return self.toc[node_id]
+
+    def get_dimension(self, column):
+        return self._column_to_dimension[column]
 
     @property
     def number_of_features(self):
